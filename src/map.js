b582602f4284e5f001c13350181b16d787d96435
Payton Lower <paytown@me.com>
2019-02-18 15:19:24 -0800

    working towards styling and figuring out input sliders

--- src/map.js
+++ src/map.js
@@ -1,9 +1,17 @@
 import forestParkOptions from './routes/forest-park.js';
 
 const runContainerAll = document.getElementById('run-container-all');
+const userNameDisplay = document.getElementById('user-name-display');
 const search = window.location.search;
 const searchParams = new URLSearchParams(search);
 const routeUrl = searchParams.get('destination');
+const userJson = window.localStorage.getItem('user');
+
+if(!userJson) {
+    window.location = './index.html';
+}
+const user = JSON.parse(userJson);
+userNameDisplay.textContent = user.name;
 
 let routeOptions = null;
 
@@ -16,7 +24,7 @@ for(let i = 0; i < routeOptions.length; i++) {
     console.log(route);
 
     const runImage = document.createElement('img');
-    runImage.src = './assets/fp-marathon1.jpg';
+    runImage.src = route.imageMapPage;
     runImage.alt = 'image of run';
     runImage.classList.add('display-photo');
     const imageContainer = document.createElement('section');
@@ -27,9 +35,9 @@ for(let i = 0; i < routeOptions.length; i++) {
     const runName = document.createElement('h3');
     runName.textContent = route.name;
     const distance = document.createElement('h4');
-    distance.textContent = route.distance;
+    distance.textContent = route.distance + ' miles';
     const elevation = document.createElement('h4');
-    elevation.textContent = route.elevation;
+    elevation.textContent = route.elevation + ' feet';
     const description = document.createElement('p');
     description.textContent = route.description;
     const runInfoContainer = document.createElement('section');
@@ -63,36 +71,3 @@ for(let i = 0; i < routeOptions.length; i++) {
 
     runContainerAll.appendChild(runIcons);
 }
-
-
-// build a route obj 
-// display routes on map page using DOM
-// implement filters
-
-
-
-
-
-
-// const forestParkMarathon = {
-//     id: 'fp-marathon',
-//     name: 'Forest Park Marathon',
-//     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sodales ex quis arcu accumsan sodales. Integer eget felis in augue condimentum vehicula. Proin pretium finibus erat, et placerat arcu. In hac habitasse platea dictumst. Nunc porttitor ornare leo in dapibus. Aliquam nec tincidunt risus. Curabitur ac ullamcorper ipsum. Mauris at.',
-//     distance: '26.38',
-//     elevation: '100000000',
-//     favorite: false,
-//      completed: ,
-//     imageOne: './assets/fp-marathon1.jpg',
-//     imageTwo: '',
-//     imageThree: '',
-//     thumbnailMap: './assets/fp-marathon-map-thumb.jpg',
-//     largeMap: '',
-//     extraInfo: '',
-//     bathroom: false,
-//     familyFriendly: false,
-//     // completed to be found in user object
-//     gpsCoordinates: '12.345 67.890'
-// };
-
-
-
