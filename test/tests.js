05cb5015cb2f808d1bd0bf64316f5769ad507495
mcnadams <mcneilbo@oregonstate.edu>
2019-03-04 14:37:41 -0800

    makeRouteDetail function created without interpolation- static only

--- test/tests.js
+++ test/tests.js
@@ -80,7 +80,7 @@ const route = {
 };
 
 
-QUnit.test("forest park template", assert => {
+QUnit.test('forest park template', assert => {
     const expected = /* html */ `
         <section class="run-container">
 
@@ -106,6 +106,91 @@ QUnit.test("forest park template", assert => {
     `;
 
     const result = makeHtmlTemplate(route);
+
+    assert.htmlEqual(result, expected);
+});
+
+function makeRouteDetail(route) {
+    const template = document.createElement('template');
+    template.innerHTML = `
+        <main>
+            <section class="route-map">
+                <img src="assets/working/FP-MarathonTwo.svg" alt="route map" id="route-map">
+            </section>
+
+            <section id="elevation-details">
+                <img src="assets/elevation-profile/fp-marathon-elevation.svg" alt="elevation chart" id="elevation-chart">
+            </section>
+
+            <section id="route-details">
+                <section class="route-details-container">
+                    <h3>Forest Park Marathon</h3>
+                    <h4>26.38 miles</h4>
+                    <h4>3200 feet</h4>
+                    <h5>519280E 504589N</h5>
+                    <p>This route will take you across 2/3 of the length of Forest Park, through some of the less busy areas, and along some of the most scenic parts of Wildwood Trail. Note that this route is not a loop! It begins with a climb from the Lower Saltzman Road trailhead and ends at Lower Macleay Park.</p>
+                </section>
+            </section>
+
+            <section id="detail-photo-section">
+                <section id="detail-photo-flex">
+                    <img class="detail-photo" id="detail-photo-1" src="assets/images/lowerpark/lowermacleay/LM2.jpg">
+                    <img class="detail-photo" id="detail-photo-2" src="assets/images/lowerpark/lowermacleay/LM6.jpg">
+                    <img class="detail-photo" id="detail-photo-3" src="assets/images/lowerpark/keil.jpg">
+                    <img class="detail-photo" id="detail-photo-4" src="assets/images/midpark/trillium.jpg">
+                </section>
+            </section>
+
+            <section id="more-info-container">
+                <section id="more-info-section">
+                    <h4>More Info</h4>
+                    <p>Access the starting trailhead on Lower Saltzman Road off of Highway 30. Limited street parking is available. The route ends at Lower Macleay Park, which has restrooms, parking, and a water fountain.</p>
+                </section>
+            </section>
+        </main>
+    `;
+    return template.content;
+}
+
+QUnit.test('forest park marathon route detail template', assert => {
+    const expected = `
+        <main>
+            <section class="route-map">
+                <img src="assets/working/FP-MarathonTwo.svg" alt="route map" id="route-map">
+            </section>
+
+            <section id="elevation-details">
+                <img src="assets/elevation-profile/fp-marathon-elevation.svg" alt="elevation chart" id="elevation-chart">
+            </section>
+
+            <section id="route-details">
+                <section class="route-details-container">
+                    <h3>Forest Park Marathon</h3>
+                    <h4>26.38 miles</h4>
+                    <h4>3200 feet</h4>
+                    <h5>519280E 504589N</h5>
+                    <p>This route will take you across 2/3 of the length of Forest Park, through some of the less busy areas, and along some of the most scenic parts of Wildwood Trail. Note that this route is not a loop! It begins with a climb from the Lower Saltzman Road trailhead and ends at Lower Macleay Park.</p>
+                </section>
+            </section>
+
+            <section id="detail-photo-section">
+                <section id="detail-photo-flex">
+                    <img class="detail-photo" id="detail-photo-1" src="assets/images/lowerpark/lowermacleay/LM2.jpg">
+                    <img class="detail-photo" id="detail-photo-2" src="assets/images/lowerpark/lowermacleay/LM6.jpg">
+                    <img class="detail-photo" id="detail-photo-3" src="assets/images/lowerpark/keil.jpg">
+                    <img class="detail-photo" id="detail-photo-4" src="assets/images/midpark/trillium.jpg">
+                </section>
+            </section>
+
+            <section id="more-info-container">
+                <section id="more-info-section">
+                    <h4>More Info</h4>
+                    <p>Access the starting trailhead on Lower Saltzman Road off of Highway 30. Limited street parking is available. The route ends at Lower Macleay Park, which has restrooms, parking, and a water fountain.</p>
+                </section>
+            </section>
+        </main>
+    `;
+    const result = makeRouteDetail(route);
 
     assert.htmlEqual(result, expected);
 });
