48d4aedc4cdeb4b16e01d1ac87bbebe784fa328a
mcnadams <mcneilbo@oregonstate.edu>
2019-03-12 16:25:51 -0700

    prep to add firebase authentication to index

--- src/index.js
+++ src/index.js
@@ -1,38 +1,53 @@
-const userNameForm = document.getElementById('user-name');
-const destinationForm = document.getElementById('destination-form');
-const destinationDropdown = document.getElementById('destination-dropdown');
-const userNameDisplay = document.getElementById('user-name-display');
-const userNameContainer = document.getElementById('user-name-container');
-const selectForm = document.getElementById('destination-dropdown');
+import { auth } from './firebase.js';
 
-const user = {
-    name: '',
-    age: '',
-    sex: '',
-    location: '',
-    destination: ''
-};
+const ui = new firebaseui.auth.AuthUI(auth);
 
-userNameForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    const formData = new FormData(userNameForm);
-    user.name = formData.get('name');
-    userNameForm.classList.remove('user-name');
-    userNameForm.classList.add('hidden');
-    destinationForm.hidden = false;
+ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+    signInSuccessUrl: './routes.html?destination=forest-park',
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE
+});
 
-    selectForm.classList.remove('hidden');
+
+
+// const userNameForm = document.getElementById('user-name');
+// const destinationForm = document.getElementById('destination-form');
+// const destinationDropdown = document.getElementById('destination-dropdown');
+// const userNameDisplay = document.getElementById('user-name-display');
+// const userNameContainer = document.getElementById('user-name-container');
+// const selectForm = document.getElementById('destination-dropdown');
+
+// const user = {
+//     name: '',
+//     age: '',
+//     sex: '',
+//     location: '',
+//     destination: ''
+// };
+
+// userNameForm.addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     const formData = new FormData(userNameForm);
+//     user.name = formData.get('name');
+//     userNameForm.classList.remove('user-name');
+//     userNameForm.classList.add('hidden');
+//     destinationForm.hidden = false;
+
+//     selectForm.classList.remove('hidden');
     
-    userNameContainer.hidden = false;
-    userNameDisplay.textContent = user.name;
-});
+//     userNameContainer.hidden = false;
+//     userNameDisplay.textContent = user.name;
+// });
 
-destinationDropdown.addEventListener('change', function() {
-    const formData = new FormData(destinationForm);
-    const destinationChoice = formData.get('destination-dropdown');
-    user.destination = destinationChoice;
+// destinationDropdown.addEventListener('change', function() {
+//     const formData = new FormData(destinationForm);
+//     const destinationChoice = formData.get('destination-dropdown');
+//     user.destination = destinationChoice;
 
-    const userJson = JSON.stringify(user);
-    window.localStorage.setItem('user', userJson);
-    window.location = './map.html?destination=' + encodeURIComponent(destinationChoice);
-});
+//     const userJson = JSON.stringify(user);
+//     window.localStorage.setItem('user', userJson);
+//     window.location = './map.html?destination=' + encodeURIComponent(destinationChoice);
+// });
