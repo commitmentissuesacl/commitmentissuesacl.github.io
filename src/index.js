aef0a280fcabc4571bb36695fc8396e862152afd
mcnadams <mcneilbo@oregonstate.edu>
2019-03-13 17:38:47 -0700

    cleanup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import { auth, usersRef } from './firebase/firebase.js';
 
 const ui = new firebaseui.auth.AuthUI(auth);
 
+auth.onAuthStateChanged(user => {
+    if(user){
+        window.location = 'routes.html';
+    }
+});
+
 ui.start('#firebaseui-auth-container', {
     signInOptions: [
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
@@ -12,7 +18,6 @@ ui.start('#firebaseui-auth-container', {
     callbacks: {
         signInSuccessWithAuthResult(authResult) {
             const user = authResult.user;
-            console.log(user);
             usersRef.child(user.uid)
                 .set({
                     uid: user.uid,
@@ -23,44 +28,3 @@ ui.start('#firebaseui-auth-container', {
         }
     }
 });
-
-
-
-// const userNameForm = document.getElementById('user-name');
-// const destinationForm = document.getElementById('destination-form');
-// const destinationDropdown = document.getElementById('destination-dropdown');
-// const userNameDisplay = document.getElementById('user-name-display');
-// const userNameContainer = document.getElementById('user-name-container');
-// const selectForm = document.getElementById('destination-dropdown');
-
-// const user = {
-//     name: '',
-//     age: '',
-//     sex: '',
-//     location: '',
-//     destination: ''
-// };
-
-// userNameForm.addEventListener('submit', function(event) {
-//     event.preventDefault();
-//     const formData = new FormData(userNameForm);
-//     user.name = formData.get('name');
-//     userNameForm.classList.remove('user-name');
-//     userNameForm.classList.add('hidden');
-//     destinationForm.hidden = false;
-
-//     selectForm.classList.remove('hidden');
-    
-//     userNameContainer.hidden = false;
-//     userNameDisplay.textContent = user.name;
-// });
-
-// destinationDropdown.addEventListener('change', function() {
-//     const formData = new FormData(destinationForm);
-//     const destinationChoice = formData.get('destination-dropdown');
-//     user.destination = destinationChoice;
-
-//     const userJson = JSON.stringify(user);
-//     window.localStorage.setItem('user', userJson);
-//     window.location = './map.html?destination=' + encodeURIComponent(destinationChoice);
-// });
