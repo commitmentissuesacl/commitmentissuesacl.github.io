13b5de87c9025a9aa81e8b788f90d5d8a438e4d5
mcnadams <mcneilbo@oregonstate.edu>
2019-03-15 09:30:36 -0700

    update user profile

--- src/runner-profile.js
+++ src/runner-profile.js
@@ -8,12 +8,19 @@ const fileUpload = document.getElementById('avatar-upload');
 const imageDisplay = document.getElementById('image-display');
 const photoForm = document.getElementById('photo-form');
 
+const name = document.getElementById('input-name');
+const age = document.getElementById('age');
+const gender = document.getElementById('gender');
+const location = document.getElementById('location');
+const form = document.getElementById('user-info');
+
 auth.onAuthStateChanged(user => {
     if(!user) {
         window.location = 'routes.html';
     }
 
     else {
+        //get custom photo from user database info
         usersRef.child(user.uid).once('value')
             .then(snapshot => {
                 const value = snapshot.val();
@@ -26,17 +33,17 @@ auth.onAuthStateChanged(user => {
                     imageDisplay.classList.add('hidden');
                 }
             });
-
+        //get file from user
         fileUpload.addEventListener('change', e => {
             const file = e.target.files[0];
-
+            //add file to storage
             photoForm.addEventListener('submit', event => {
                 event.preventDefault();
                 const folderName = user.uid + '/';
                 const fileName = 'avatar';
                 const ref = firebase.storage().ref(folderName + fileName);
                 const uploadTask = ref.put(file);
-    
+                //add reference to photo to user database
                 uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, 
                     function() {
                         uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
@@ -52,32 +59,29 @@ auth.onAuthStateChanged(user => {
             });
 
         });
+
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            const formData = new FormData(form);
+        
+            usersRef.child(user.uid)
+                .update({
+                    age: formData.get('age'),
+                    gender: formData.get('gender'),
+                    location: formData.get('location')
+                });
+            window.alert('User info updated');     
+        });
+
     }
 
 });
 
 
-// const name = document.getElementById('input-name');
-// const age = document.getElementById('age');
-// const sex = document.getElementById('sex');
-// const location = document.getElementById('location');
-// const form = document.getElementById('user-info');
 
+// set form values with existing data
 // name.value = user.name;
 // age.value = user.age;
-// sex.value = user.sex;
+// gender.value = user.gender;
 // location.value = user.location;
 
-// form.addEventListener('submit', function(event) {
-//     event.preventDefault();
-//     const formData = new FormData(form);
-//     user.name = formData.get('input-name');
-//     user.age = formData.get('age');
-//     user.sex = formData.get('sex');
-//     user.location = formData.get('location');
-
-//     const jsonUser = JSON.stringify(user);
-//     window.localStorage.setItem('user', jsonUser);
-
-//     window.history.back();
-// });
