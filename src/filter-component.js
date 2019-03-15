1cd9cb5bd1e0789672844219aee451d4db36f1eb
mcnadams <mcneilbo@oregonstate.edu>
2019-03-14 17:47:47 -0700

    add filters to route pages $#^$

--- src/filter-component.js
+++ src/filter-component.js
@@ -1,28 +1,29 @@
+import { loadRoutes } from './make-html-template.js';
 
-export function filterByMaxMiles(routes, maxMiles) {
-    return routes.filter(route => route.distance <= maxMiles);
+export function filterByMaxMiles(routes, max) {
+    return routes.filter(route => parseFloat(route.distance) < parseFloat(max));
 }
 
-export function filterByMinMiles(routes, minMiles) {
-    return routes.filter(route => route.distance >= minMiles);
+export function filterByMinMiles(routes, min) {
+    return routes.filter(route => parseFloat(route.distance) > parseFloat(min));
 }
 
-export function filterByDistance(routes, minMiles, maxMiles) {
-    const filteredByMin = filterByMinMiles(routes, minMiles);
-    return filterByMaxMiles(filteredByMin, maxMiles);
+export function filterByDistance(routes, min, max) {
+    const filteredByMin = filterByMinMiles(routes, min);
+    return filterByMaxMiles(filteredByMin, max);
 }
 
-export function filterByMinElevation(routes, minElev) {
-    return routes.filter(route => route.elevation >= minElev);
+export function filterByMinElevation(routes, min) {
+    return routes.filter(route => parseInt(route.elevation) > parseInt(min));
 }
 
-export function filterByMaxElevation(routes, maxElev) {
-    return routes.filter(route => route.elevation <= maxElev);
+export function filterByMaxElevation(routes, max) {
+    return routes.filter(route => parseInt(route.elevation) < parseInt(max));
 }
 
-export function filterByElevation(routes, minElev, maxElev) {
-    const filteredByMin = filterByMinElevation(routes, minElev);
-    return filterByMaxElevation(filteredByMin, maxElev);
+export function filterByElevation(routes, min, max) {
+    const filteredByMin = filterByMinElevation(routes, min);
+    return filterByMaxElevation(filteredByMin, max);
 }
 
 export function filterByBathrooms(routes) {
@@ -31,4 +32,41 @@ export function filterByBathrooms(routes) {
 
 export function filterByFamily(routes) {
     return routes.filter(route => route.familyFriendly);
+}
+
+const minMiles = document.getElementById('min-miles');
+const maxMiles = document.getElementById('max-miles');
+const minElevation = document.getElementById('min-elevation');
+const maxElevation = document.getElementById('max-elevation');
+const bathroomFilter = document.getElementById('bathroom');
+const familyFilter = document.getElementById('family');
+
+export function filterRoutes(allRoutes) {
+    let routes = allRoutes;
+    const min = minMiles.value;
+    const max = maxMiles.value;
+    const minElev = minElevation.value;
+    const maxElev = maxElevation.value;
+
+    if(bathroomFilter.checked) {
+        routes = filterByBathrooms(routes);
+    }
+
+    if(familyFilter.checked) {
+        routes = filterByFamily(routes);
+    }
+
+    routes = filterByElevation(routes, minElev, maxElev);
+    routes = filterByDistance(routes, min, max);
+    loadRoutes(routes);
+}
+
+export default function loadFilter(routes) {
+    bathroomFilter.addEventListener('change', () => { filterRoutes(routes); });
+    familyFilter.addEventListener('change', () => { filterRoutes(routes); });
+    minMiles.addEventListener('change', () => { filterRoutes(routes); });
+    maxMiles.addEventListener('change', () => { filterRoutes(routes); });
+    minElevation.addEventListener('change', () => { filterRoutes(routes); });
+    maxElevation.addEventListener('change', () => { filterRoutes(routes); });
+
 }
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import forestParkOptions from './routes/forest-park.js';
 import otherOptions from './routes/other.js';
 import { loadRoutes } from './make-html-template.js';
 import { loadHeader, loadFooter } from './header-template.js';
+import loadFilter from './filter-component.js';
 
 
 const search = window.location.search;
@@ -24,10 +25,4 @@ else {
 }
 
 loadRoutes(routeOptions);
-
-const minMiles = document.getElementById('min-miles');
-const maxMiles = document.getElementById('max-miles');
-const minElevation = document.getElementById('min-elevation');
-const maxElevation = document.getElementById('max-elevation');
-const bathroomFilter = document.getElementById('bathroom');
-const familyFilter = document.getElementById('family');
+loadFilter(routeOptions);
